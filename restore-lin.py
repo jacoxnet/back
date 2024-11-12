d5f13dd3e67c5ba7adfacc5ea32669718535ac66
James A. Cox <jacox@jacox.net>
2024-11-11 19:09:47 -0600

    delete log from restore lin

--- restore-lin.py
+++ restore-lin.py
@@ -1,19 +1,20 @@
 import os;
 
 #dirs_to_process = ["desktop", "documents", "downloads", "music", "pictures", "updates", "videos", "bin", "code"]
-dirs_to_process = ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Updates", "Videos"]
+dirs_to_process = ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Updates", "Videos", "bin"]
 
 
 command = "/usr/bin/rsync"
 argfrom = " /mnt/nas/jdrive/Users/jacox/"
 argto = " /home/jacox/"
 opts = " -avz"
-logfile = " /home/jacox/.restorelog"
+#logfile = " /home/jacox/.restorelog"
 
 for count, dname in enumerate(dirs_to_process):
     print("processing ", count, ": ", dname)
-    log = ">" + logfile if count == 0 else ">>" + logfile
-    full_command = command + opts + argfrom + dname + "/" + argto + dname + "/" + log
+    #log = ">" + logfile if count == 0 else ">>" + logfile
+    #full_command = command + opts + argfrom + dname + "/" + argto + dname + "/" + log
+    full_command = command + opts + argfrom + dname + "/" + argto + dname + "/"
     print("full command:", full_command)
     os.system(full_command)
 
